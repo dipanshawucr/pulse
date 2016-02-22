770ebab752b0161e22457058c9fc3662106e6311
WonJune Tai <wonjune.tai@gmail.com>
2016-02-22 17:19:15 -0500

    Modifying samtools

--- for_preprocess/samtools.py
+++ for_preprocess/samtools.py
@@ -1,7 +1,6 @@
-import os
+import sys
+import getopt
 import subprocess
-
-PULSE_PATH = os.getcwd()
 
 
 def run_samtools(pulse_path, cell_line_for_samtools):
@@ -12,11 +11,35 @@ def run_samtools(pulse_path, cell_line_for_samtools):
     output_file = open(pulse_path + '/output/for_preprocess/' +
                        cell_line_for_samtools + '/no-rg/' + cell_line_for_samtools, "w")
 
-    print "Running: view"
+    print("Running: view")
     p1 = subprocess.Popen(command1, stdout=subprocess.PIPE)
-    print "Running: grep"
+    print("Running: grep")
     p2 = subprocess.Popen(command2, stdin=p1.stdout, stdout=subprocess.PIPE)
-    print "Running view2"
+    print("Running view2")
     p3 = subprocess.Popen(command3, stdin=p2.stdout, stdout=output_file)
-    exit_codes = [p.wait() for p in p1, p2, p3]
+    exit_codes = [p.wait() for p in [p1, p2, p3]]
     return exit_codes
+
+
+def main(argv):
+    pulse_path = ''
+    cell_line_for_samtools = ''
+
+    try:
+        opts, args = getopt.getopt(argv, "hp:c:", ["path=", "cell_line="])
+    except getopt.GetoptError:
+        print('samtools.py -p <pulse_path> -c <cell_line_for_samtools>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('samtools.py -p <pulse_path> -c <cell_line_for_samtools>')
+            sys.exit()
+        elif opt in ("-p", "--path"):
+            pulse_path = arg
+        elif opt in ("-c", "--cell-line"):
+            cell_line_for_samtools = arg
+    print(pulse_path, cell_line_for_samtools)
+    run_samtools(pulse_path, cell_line_for_samtools)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- submit_job.py
+++ submit_job.py
@@ -4,15 +4,23 @@
 import subprocess
 
 
-def submit_new_job(script_name, list_of_args):
+def submit_new_job(script_name, list_of_args=[]):
+    """
+    Submits a new job to beagle with a max runtime for 48 hours,
+    with 4 GB of memory, and 4 cores per job.
+    Returns 0 upon task completion.
 
+    :param script_name:
+    :param list_of_args:
+    :return:
+    """
     command1 = ['submitjob',
                 '48',
                 '-m',
                 '4',
                 '-c',
                 '4',
-                script_name]
+                script_name] + list_of_args
     p1 = subprocess.Popen(command1)
     exit_code = p1.wait()
     return exit_code
